143d310dbca0886950db004297139115e37e7989
Vini Dalvino <37189791+ViniDalvino@users.noreply.github.com>
2021-12-03 14:42:29 -0500

    first commit

---
+++ day-1/day-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+int main(int argc, char *argv[])
+{
+  FILE *challenge_input_file;
+  challenge_input_file = fopen("../day-1/input.txt", "r")    
+
+  return 0; 
+}
